f269c7b9285a9728f04e625cdbfab73ac934a966
yingjiac <yingjiac@uchicago.edu>
2022-04-19 20:38:08 +0000

    Finish the task with 100%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,55 @@
 class ApplicationController < ActionController::Base  # Inherit from ActionController this Class
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end 
+   
   def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock" 
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
     # write a ton of code
     #render({ :plain => rand(100)})
     #redirect_to("https://www.wikipedia.org")
     #render({ :html => "<h1>Hello, World!</h1>".html_safe})
-    render({ :template => "game_templates/user_rock.html.erb"}) # create a folder and also a file wihich is embedded ruby template
+    render({ :template => "game_templates/user_rock.html.erb"}) 
+    # create a folder and also a file wihich is embedded ruby template
     
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissor
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock" 
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/user_scissor.html.erb"})
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "homepage"}
+  )
+
   get("/rock", { :controller =>"application", :action => "play_rock"}
+  )
+
+  get("/paper", { :controller => "application", :action => "play_paper"}
+  )
+
+  get("/scissors", { :controller => "application", :action => "play_scissor"}
   )
 end
