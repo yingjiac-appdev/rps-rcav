7f42ca7714cec4836989667896934430bbbc5387
yingjiac <yingjiac@uchicago.edu>
2022-04-19 17:19:26 +0000

    Rock page playing works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base  # Inherit from ActionController this Class
   layout(false)
 
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # write a ton of code
+    #render({ :plain => rand(100)})
+    #redirect_to("https://www.wikipedia.org")
+    #render({ :html => "<h1>Hello, World!</h1>".html_safe})
+    render({ :template => "game_templates/user_rock.html.erb"}) # create a folder and also a file wihich is embedded ruby template
+    
+  end
 end
+
